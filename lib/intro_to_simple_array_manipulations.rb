6c77061592774a8257d6698f31e964aedd242173
passionate-optimization-7504 <nicholasjardine9@gmail.com>
2019-09-19 16:29:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -16,16 +16,4 @@ end
 
 def using_shift(array)
   array.shift 
-  array
 end
-
-def shift_with_args(array)
-  array.shift(2)
-end
-
-def using_concat(arr1, arr2)
-  arr1.concat(arr2)
-end
-
-def using_insert(arrary, element)
-  array.insert(4, element)
